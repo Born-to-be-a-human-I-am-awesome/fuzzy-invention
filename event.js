abe20296c1281e4fd2bbfe44b8ff683a7e960e9d
Born-to-be-a-human-I-am-awesome <3285809286@qq.com>
2022-02-16 22:37:10 +0800

    依赖文件

--- event.js
+++ event.js
@@ -1,8 +1,20 @@
 
 /*点击事件*/
+/*点击加群*/
+function addGroup()
+{
+    window.open("https://jq.qq.com/?_wv=1027&k=ug3CkkII")
+}
+
+function addgroupTips()
+{
+    alert("点击下方群号即可加入友军大本营审核群！")
+}
+
+/*点击菜单项*/
 function homePage()
 {
-    alert("你点了首页！")
+    alert("这里就是首页，别再点了！")
 }
 
 function synopsisPage()
@@ -34,3 +46,24 @@ function contactPage()
 {
     alert("你点了联系我们！")
 }
+
+
+/*周期事件*/
+/*统计网站运行时间*/
+function runtime(){
+        // 初始时间，月/日/年 时:分:秒
+        X = new Date("2/16/2022 22:34:00");
+        Y = new Date();
+        T = (Y.getTime()-X.getTime());
+        M = 24*60*60*1000;
+        a = T/M;
+        A = Math.floor(a);
+        b = (a-A)*24;
+        B = Math.floor(b);
+        c = (b-B)*60;
+        C = Math.floor((b-B)*60);
+        D = Math.floor((c-C)*60);
+        //信息写入到DIV中
+        span.innerHTML = "本炸图官网已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"
+    }
+    setInterval(runtime, 1000);
