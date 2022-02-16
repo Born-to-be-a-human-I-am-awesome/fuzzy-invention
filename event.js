f921b49c9c680e242d2d2e182d7e1a7e1ef621c7
Born-to-be-a-human-I-am-awesome <3285809286@qq.com>
2022-02-16 21:13:39 +0800

    网页依赖

---
+++ event.js
@@ -0,0 +1,36 @@
+
+/*点击事件*/
+function homePage()
+{
+    alert("你点了首页！")
+}
+
+function synopsisPage()
+{
+    alert("你点了公司简介！")
+}
+
+function solutionPage()
+{
+    alert("你点了解决方案！")
+}
+
+function newsPage()
+{
+    alert("你点了公司新闻！")
+}
+
+function dynamicPage()
+{
+    alert("你点了行业动态！")
+}
+
+function recruitPage()
+{
+    alert("你点了招贤纳才！")
+}
+
+function contactPage()
+{
+    alert("你点了联系我们！")
+}
